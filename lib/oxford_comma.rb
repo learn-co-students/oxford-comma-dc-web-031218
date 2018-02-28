ab3c5bd851ca02326ccbe82df7d48727d57c2015
Jerry Lee <jwl62@georgetown.edu>
2018-02-28 20:52:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    last = array.pop
+    to_return = array.join(", ")
+    to_return += ", and #{last}"
+    return to_return
+  end
 end
