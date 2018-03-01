144b8fb39721d8393f90e7fa3e255bd949ababae
Joseph DiPeppe <jkdipeppe@gmail.com>
2018-03-01 19:14:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  if array.length < 2
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_string = ", and #{array[-1]}"
+    arr_final = Array.new
+    arr_final = array
+    arr_final.pop
+    final_string = String.new
+    final_string = arr_final.join(", ")
+
+    final_string.concat(last_string)
+  end
 
 end
